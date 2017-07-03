97811148c2c4dff6f9861a5d50c9af8a1d0d2acb
Jairo Sousa <jaironsousa@gmail.com>
2017-07-02 21:45:55 -0300

    ajuste no commit anterior

---
+++ src/app/restaurants/restaurant/restautant.model.ts
@@ -0,0 +1,19 @@
+export interface Restaurant {
+
+    id: string
+    name: string
+    category: string
+    deliveryEstimate: string
+    rating: number
+    imagePath: string
+
+
+    /*"id": "bread-barery",
+    "name": "Bread-Barery",
+    "category": "Bakery",
+    "deliveryEstimate": "25m",
+    "rating": 4.9,
+    "imagePath": "assent/img/restaurants/breadbakery.png",
+    "about": "A Brend & Brakery tem 40 anos de mercado. Fazemos os melhores doces do mercado."
+    "hours": "Funciona de segunda à sexta, de 8h às 23h"*/
+}
